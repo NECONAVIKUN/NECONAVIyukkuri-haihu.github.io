da8de3faae80b2b6b4145d394bf584f825f008c6
miikun95 <miikun95@miikun95.net>
2026-03-15 13:12:40 +0900

    Add click event for reimu sound playback

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ document.getElementById("marisa").addEventListener('click", function() {
   s.currentTime = 0;
   s.play();
 });
+
+document.getElementById("reimu").addEventListener("click", function() {
+  var s = document.querySelector("reimu_s");
+  s.currentTime = 0;
+  s.play();
+});
